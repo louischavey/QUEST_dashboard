c01e7f28489bd59fdaec340f9563e2956d12218d
Louis Chavey <81273605+louischavey@users.noreply.github.com>
2024-03-07 01:24:15 -0600

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,32 +4,20 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>script dashboard</h1>
+      <form action="/upload" method="post" encType="multipart/form-data">
+        <input type="file" name="file" />
+        <input type="submit" defaultValue="Upload" />
+      </form>
+      <form action="/run" method="post" encType="multipart/form-data">
+        <input type="text" name="script_filename" />
+        <input type="submit" defaultValue="write text" />
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
